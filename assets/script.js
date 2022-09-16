6a7faddfb99ab2cce8179ecc0e400afc7016f8d8
Duran11 <antonio11duran@gmail.com>
2022-09-16 10:04:33 -0600

    added function to add background color to text boxes depending on time of day

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,36 @@
-var timeBox = document.getElementById("inputGroup-sizing-lg");
-var inputBox = document.querySelector("#input-box");
+var nineBox = document.getElementById("nine");
+var tenBox = document.getElementById("ten");
+var elevenBox = document.getElementById("eleven");
+var twelveBox = document.getElementById("twelve");
+var oneBox = document.getElementById("one");
+var twoBox = document.getElementById("two");
+var threeBox = document.getElementById("three");
+var fourBox = document.getElementById("four");
+var fiveBox = document.getElementById("five");
 
-console.log(timeBox.value);
+var arrayBox = [nineBox, tenBox, elevenBox, twelveBox, oneBox, twoBox, threeBox, fourBox, fiveBox];
 
 setInterval(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 }, 1000
 )
 
-if (timeBox.value == moment().format("hA")) {
-    inputBox.setAttribute("background-color", "red");
+var now = moment();
+var currentHour = parseInt(moment().format("H"));
+
+console.log(currentHour);
+console.log(typeof currentHour);
+
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+function activeHour() {
+    for (var h = 0; h < hours.length; h++) {
+    if (currentHour == hours[h]) {
+        for (var i = h + 1; i < arrayBox.length; i++) {
+            arrayBox[h].style.backgroundColor = "red";
+            arrayBox[i].style.backgroundColor = "green";
+        }
+    }}
 }
+
+activeHour();
